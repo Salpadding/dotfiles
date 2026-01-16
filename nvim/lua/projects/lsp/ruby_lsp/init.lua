2c985502d6ddd004dde983e746f229a7a9c5695c
admin <admin@nixos-vm>
2026-01-16 13:38:05 +0800

    enanle sorbet

--- nvim/lua/projects/lsp/ruby_lsp/init.lua
+++ nvim/lua/projects/lsp/ruby_lsp/init.lua
@@ -1,1 +1,37 @@
-return {}
+return {
+  cmd = { "bundle", "exec", "ruby-lsp" },
+  init_options = {
+    enabledFeatures = {
+      "codeActions",
+      "codeLens",
+      "completion",
+      "definition",
+      "diagnostics",
+      "documentHighlights",
+      "documentLink",
+      "documentSymbols",
+      "foldingRanges",
+      "formatting",
+      "hover",
+      "inlayHint",
+      "onTypeFormatting",
+      "selectionRanges",
+      "semanticHighlighting",
+      "signatureHelp",
+      "typeHierarchy",
+      "workspaceSymbol",
+    },
+    featuresConfiguration = {
+      inlayHint = {
+        implicitHashValue = true,
+        implicitRescue = true,
+      },
+    },
+    -- Sorbet addon is auto-enabled when sorbet-runtime is in Gemfile
+    addonSettings = {
+      ["Ruby LSP Sorbet"] = {
+        enabled = true,
+      },
+    },
+  },
+}
